78abf1eaeb4c2f6690d3cd62d7967669f2d85f47
nurlopez <nlopezcolom@gmail.com>
2019-12-04 16:18:01 +0100

    Iteration 1 + 2 OK - create FoodBox component + Display all foods

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+
 class App extends Component {
+  state = {
+    food: foods
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1 className="App-title">Iron Nutrition</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        { this.state.food.map( (oneFood) => {
+          return (
+        <FoodBox {...oneFood} />
+          )
+        })
+        }
+
         </p>
       </div>
     );
